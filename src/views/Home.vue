41f1df9157f040e64dc6b86495d6e3d9cf39b035
Sofia Premuzaj <sofia.premuzaj@gmail.com>
2021-02-09 11:02:39 +0100

    trying git pull

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import OrderConfirm from "@/components/OrderConfirm.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    OrderConfirm,
   },
 };
 </script>
